cf1fa212e7ea4e80a5042cb0a7e46a67b446538a
eyradel <60956082+eyradel@users.noreply.github.com>
2021-08-06 08:18:42 -0700

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap.css"
+          >
+    <link rel="stylesheet" href="mdb.css">
+<link rel="stylesheet" href="style.css">
+    <style>
+    .btn
+        {
+            border-radius: 50px;
+        }
+        .form-control
+    {
+        border-radius: 50px;
+
+    }
+        .card
+        {
+         border-radius: 10px;
+            background: -webkit-linear-gradient(180deg,bisque,lavender);
+        }
+    
+    </style>
+    <title>Simple PHP CRUD</title>
+</head>
+<body>
